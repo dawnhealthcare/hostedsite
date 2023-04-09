7a882f2f64e69ce9a64d331486b5817172a8d9c5
arshadnawazbaig <arshadnawazbaig@gmail.com>
2023-04-09 22:10:15 +0500

    ready to deploy

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -74,7 +74,7 @@ const HomePage = () => {
                 <iframe
                   width="100%"
                   height="603"
-                  src="https://www.youtube.com/embed/iilnY08udm0"
+                  src="https://www.youtube.com/embed/zdDU42ldncw"
                   title="YouTube video player"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   allowFullScreen
