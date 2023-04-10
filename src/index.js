2358228bd53631bea9e93dcf1a436c9d78e9c572
arshadnawazbaig <arshadnawazbaig@gmail.com>
2023-04-11 01:50:37 +0500

    latset changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/hostedsite">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
